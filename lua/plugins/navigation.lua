0cde2da02e71b95cc5615fdad8b25e7da1de2060
Logan Roberts <logancroberts@outlook.com>
2023-12-16 10:11:12 -0500

    Shift bufferline over for neotree

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -11,10 +11,17 @@ local M = {
       options = {
         diagnostics_indicator = function(_, _, diag)
           local icons = require('lazyvim.config').icons.diagnostics
-          local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') ..
-          (diag.warning and icons.Warn .. diag.warning or '')
+          local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') .. (diag.warning and icons.Warn .. diag.warning or '')
           return vim.trim(ret)
         end,
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Neo-tree',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
       },
     },
     config = function(_, opts)
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -47,6 +47,9 @@ return {
           hide_dotfiles = false,
         },
       },
+      window = {
+        width = 30,
+      },
     },
   },
 
@@ -143,11 +146,11 @@ return {
         },
         -- you can enable a preset for easier configuration
         presets = {
-          bottom_search = true,         -- use a classic bottom cmdline for search
-          command_palette = true,       -- position the cmdline and popupmenu together
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false,       -- add a border to hover docs and signature help
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
         },
       }
     end,
