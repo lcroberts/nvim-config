9417852d5d5ed1b424e9f6cf6358ad267a483cd1
Logan Roberts <logancroberts@outlook.com>
2023-12-29 18:49:24 -0500

    Fix some errors

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -15,8 +15,7 @@ local M = {
         always_show_bufferline = false,
         diagnostics_indicator = function(_, _, diag)
           local icons = require('lazyvim.config').icons.diagnostics
-          local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') ..
-          (diag.warning and icons.Warn .. diag.warning or '')
+          local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') .. (diag.warning and icons.Warn .. diag.warning or '')
           return vim.trim(ret)
         end,
         offsets = {
