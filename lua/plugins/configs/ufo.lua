80454646629afb0851ac95e54b8217195facf1fd
Logan Roberts <logancroberts@outlook.com>
2024-01-29 11:14:15 -0500

    Remove foldcolumn visibility

--- lua/plugins/configs/ufo.lua
+++ lua/plugins/configs/ufo.lua
@@ -28,7 +28,7 @@ end
 
 vim.o.fillchars = [[eob: ,fold: ,foldopen:⮟,foldsep: ,foldclose:⮞]]
 -- Fold column and foldnestmax need to be the same to prevent ugly numbers from appearing
-vim.o.foldcolumn = 'auto:9'
+vim.o.foldcolumn = '0'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
