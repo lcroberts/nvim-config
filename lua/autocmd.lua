4b1d0058531c6026be1b0cec4841db694738efcf
Logan Roberts <logancroberts@outlook.com>
2024-02-11 10:31:40 -0500

    Help makes vertical split instead of horizontal split.

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -47,3 +47,14 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.buflisted = false
   end,
 })
+
+-- Open help window in a vertical split to the right.
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  group = vim.api.nvim_create_augroup('help_window_right', {}),
+  pattern = { '*.txt' },
+  callback = function()
+    if vim.o.filetype == 'help' then
+      vim.cmd.wincmd 'L'
+    end
+  end,
+})
