6864a76dbf04f67d54e9d2351fa372ce0bd0f925
Logan Roberts <logancroberts@outlook.com>
2024-03-18 15:25:40 -0400

    Remove format on save

--- lua/plugins/configs/null-ls.lua
+++ lua/plugins/configs/null-ls.lua
@@ -1,5 +1,4 @@
 local null_ls = require 'null-ls'
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 null_ls.setup {
   sources = {
@@ -25,19 +24,4 @@ null_ls.setup {
     null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.alejandra,
   },
-
-  on_attach = function(client, bufnr)
-    if client.supports_method 'textDocument/formatting' then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-          vim.lsp.buf.format { async = false }
-        end,
-      })
-    end
-  end,
 }
