79bb72f6dc5578f3d5321b377fe9d8eef92dc1d9
Logan Roberts <logancroberts@outlook.com>
2024-04-28 14:48:20 -0400

    Add autopairing back for several characters

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -143,17 +143,7 @@ return {
       require('mini.pairs').setup {
         modes = { insert = true, command = false, terminal = false },
         mappings = {
-          ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
-          ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
-          ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
-
-          [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
-          [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
-          ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
-
-          ['"'] = false,
           ["'"] = false,
-          ['`'] = false,
         },
       }
       vim.keymap.set('n', 's', '<Nop>', {})
