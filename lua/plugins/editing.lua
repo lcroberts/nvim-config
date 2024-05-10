1ebe6986bc4b3b8f4ef8bb60d6a2692f54552371
Logan Roberts <logancroberts@outlook.com>
2024-05-10 10:42:15 -0400

    Add rainbow-delimiters

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -10,6 +10,13 @@ return {
   'mbbill/undotree',
 
   {
+    url = 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim',
+    config = function()
+      require('rainbow-delimiters.setup').setup {}
+    end,
+  },
+
+  {
     'williamboman/mason.nvim',
     event = 'VeryLazy',
     opts = {
