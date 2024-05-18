a9b65390c6b24be080b9cb1c33703ce4cd24809f
Logan Roberts <logancroberts@outlook.com>
2024-05-18 07:51:47 -0400

    Remove unnecessary lspconfig options

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -17,83 +17,26 @@ lspconfig.bashls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { 'sh' },
-  root_dir = util.find_git_ancestor(),
-  single_file_support = true,
 }
 
 lspconfig.pylsp.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { 'python' },
-  root_dir = util.find_git_ancestor(),
 }
 
 lspconfig.yamlls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { 'yaml', 'yaml.docker.compose' },
-  root_dir = util.find_git_ancestor(),
 }
 
 lspconfig.tailwindcss.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {
-    'aspnetcorerazor',
-    'astro',
-    'astro-markdown',
-    'blade',
-    'clojure',
-    'django-html',
-    'htmldjango',
-    'edge',
-    'eelixir',
-    'elixir',
-    'ejs',
-    'erb',
-    'eruby',
-    'gohtml',
-    'haml',
-    'handlebars',
-    'hbs',
-    'html',
-    'html-eex',
-    'heex',
-    'jade',
-    'leaf',
-    'liquid',
-    'markdown',
-    'mdx',
-    'mustache',
-    'njk',
-    'nunjucks',
-    'php',
-    'razor',
-    'slim',
-    'twig',
-    'css',
-    'less',
-    'postcss',
-    'sass',
-    'scss',
-    'stylus',
-    'sugarss',
-    'javascript',
-    'javascriptreact',
-    'reason',
-    'rescript',
-    'typescript',
-    'typescriptreact',
-    'vue',
-    'svelte',
-  },
 }
 
 lspconfig.taplo.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { 'toml' },
-  root_dir = util.root_pattern('*.toml', '.git'),
 }
 
 lspconfig.clangd.setup {
@@ -102,7 +45,6 @@ lspconfig.clangd.setup {
     on_attach(client, bufnr)
   end,
   capabilities = capabilities,
-  root_dir = util.root_pattern('.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git'),
 }
 
 lspconfig.lua_ls.setup {
