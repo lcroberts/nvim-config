c2cbccc00a865ed8af04ffce75e55802c6eb789d
Logan Roberts <logancroberts@outlook.com>
2024-05-19 13:42:47 -0400

    Move settings to separate file

--- init.lua
+++ init.lua
@@ -1,77 +1,4 @@
--- Set <space> as the leader keyinit
--- See `:help mapleader`
-
--- [[ Setting options ]]
-local opt = vim.opt
-
-opt.whichwrap:append '<>[]hl'
-
-opt.termguicolors = true
-
--- Set highlight on search
-opt.hlsearch = true
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable relative line numbers
-opt.relativenumber = true
--- Enable mouse mode
-opt.mouse = 'a'
-
--- Enable break indent
-opt.breakindent = true
--- Visual indicators
-opt.colorcolumn = '80'
--- Save undo history
-opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-opt.ignorecase = true
-opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-opt.splitright = true
-
--- Decrease update time
-opt.updatetime = 250
-opt.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-opt.completeopt = 'menuone,noselect,preview'
-
--- Tab stuff
-opt.tabstop = 4 -- A TAB character looks like 4 spaces
-opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-opt.shiftwidth = 0
-opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-opt.smarttab = true
-
-vim.g.editorconfig = true
-vim.g.editorconfig_trim_trailing_whitespace = true
-vim.g.editorconfig_insert_final_newline = false
-
-opt.list = true
-opt.pumblend = 10 -- Popup opacity
-opt.pumheight = 10 -- Number of visible items in popum menu
-opt.scrolloff = 5 -- Keep a number of lines for context
-opt.sidescrolloff = 10 -- Columns of context
-opt.shiftround = true -- Round indent
-opt.showmode = false -- Status line does this
-opt.smartindent = true -- Insert mode indents automatically
-opt.spelllang = { 'en' } -- Spell check language
-opt.virtualedit = 'block' -- Cursor can move to where there is no character in visual block mode
-opt.wildmode = 'longest:full,full' -- Command line completion mode
-opt.winminwidth = 5 -- Minimum window width
-opt.nrformats = 'bin,hex,alpha'
-
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- disable nvim intro
-opt.shortmess:append 'sI'
+require 'settings'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
@@ -87,7 +14,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   }
 end
-opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy file event
 local Event = require 'lazy.core.handler.event'
@@ -98,6 +25,7 @@ Event.mappings['User LazyFile'] = Event.mappings.LazyFile
 require('lazy').setup {
   import = 'plugins',
 }
+
 require 'mappings'
 
 -- Create autocmds
