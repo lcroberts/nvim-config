37caa59f08c79b736796c84d13478527b5a7679e
Logan Roberts <logancroberts@outlook.com>
2024-06-05 13:36:41 -0400

    Fix some options for weird formatter behavior

--- plugin/options.lua
+++ plugin/options.lua
@@ -5,7 +5,7 @@ local vim = vim
 local opt = vim.opt
 
 opt.wrap = false
-opt.whichwrap:append("<>[]hl")
+opt.whichwrap:append '<>[]hl'
 
 opt.termguicolors = true
 
@@ -18,12 +18,12 @@ vim.wo.number = true
 -- Enable relative line numbers
 opt.relativenumber = true
 -- Enable mouse mode
-opt.mouse = "a"
+opt.mouse = 'a'
 
 -- Enable break indent
 opt.breakindent = true
 -- Visual indicators
-opt.colorcolumn = "120"
+opt.colorcolumn = '120'
 -- Save undo history
 opt.undofile = true
 
@@ -32,7 +32,7 @@ opt.ignorecase = true
 opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = "yes:2"
+vim.wo.signcolumn = 'yes:2'
 opt.splitright = true
 opt.splitbelow = true
 
@@ -41,12 +41,12 @@ opt.updatetime = 250
 opt.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-opt.completeopt = "menuone,noselect,preview"
+opt.completeopt = 'menuone,noselect,preview'
 
 -- Tab stuff
 opt.tabstop = 4 -- A TAB character looks like 4 spaces
 opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-opt.shiftwidth = 0
+opt.shiftwidth = 4 -- If 0 then tabstop is used
 opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 opt.smarttab = true
 
@@ -55,7 +55,7 @@ vim.g.editorconfig_trim_trailing_whitespace = true
 vim.g.editorconfig_insert_final_newline = false
 
 opt.list = true
-opt.fillchars:append({ eob = " " })
+opt.fillchars:append { eob = ' ' }
 
 opt.pumblend = 10 -- Popup opacity
 opt.pumheight = 10 -- Number of visible items in popum menu
@@ -64,14 +64,14 @@ opt.sidescrolloff = 8 -- Columns of context
 opt.shiftround = true -- Round indent
 opt.showmode = false -- Status line does this
 opt.smartindent = true -- Insert mode indents automatically
-opt.spelllang = { "en" } -- Spell check language
-opt.virtualedit = "block" -- Cursor can move to where there is no character in visual block mode
-opt.wildmode = "longest:full,full" -- Command line completion mode
+opt.spelllang = { 'en' } -- Spell check language
+opt.virtualedit = 'block' -- Cursor can move to where there is no character in visual block mode
+opt.wildmode = 'longest:full,full' -- Command line completion mode
 opt.winminwidth = 5 -- Minimum window width
-opt.nrformats = "bin,hex,alpha"
+opt.nrformats = 'bin,hex,alpha'
 
 opt.undofile = true
 opt.confirm = true
 
 -- disable nvim intro
-opt.shortmess:append("sI")
+opt.shortmess:append 'sI'
