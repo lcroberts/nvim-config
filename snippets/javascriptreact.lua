f31423a10b679e6872833f9fc8b03c6ff5c6c38a
Logan Roberts <logancroberts@outlook.com>
2024-06-07 12:36:43 -0400

    Move to lua snippets

---
+++ snippets/javascriptreact.lua
@@ -0,0 +1,81 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require 'luasnip.util.events'
+local ai = require 'luasnip.nodes.absolute_indexer'
+local extras = require 'luasnip.extras'
+local l = extras.lambda
+local rep = extras.rep
+local p = extras.partial
+local m = extras.match
+local n = extras.nonempty
+local dl = extras.dynamic_lambda
+local fmt = require('luasnip.extras.fmt').fmt
+local fmta = require('luasnip.extras.fmt').fmta
+local conds = require 'luasnip.extras.expand_conditions'
+local postfix = require('luasnip.extras.postfix').postfix
+local types = require 'luasnip.util.types'
+local parse = require('luasnip.util.parser').parse_snippet
+local ms = ls.multi_snippet
+local k = require('luasnip.nodes.key_indexer').new_key
+
+local function get_filename()
+  local file = vim.fn.expand '%'
+  return string.sub(file, 1, string.len(file) - 4)
+end
+
+return {
+  s(
+    { trig = 'rfc', name = 'react functional component' },
+    fmt(
+      [[
+		import React from 'react'
+
+		export default function {1}({2}) {{
+			return (
+				<div>
+					{3}
+				</div>
+			)
+		}}
+	  ]],
+      {
+        i(1, get_filename()),
+        i(2),
+        i(3),
+      }
+    )
+  ),
+  s(
+    { trig = 'rfce', name = 'react functional component export' },
+    fmt(
+      [[
+		import React from 'react'
+
+		function {1}({2}) {{
+			return (
+				<div>
+					{3}
+				</div>
+			)
+		}}
+
+		export default {}
+  	  ]],
+      {
+
+        i(1, get_filename()),
+        i(2),
+        i(3),
+        rep(1),
+      }
+    )
+  ),
+}
