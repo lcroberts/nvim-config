4db8e6b417aaf8e464e17e051704742f99d6d42f
Logan Roberts <logancroberts@outlook.com>
2024-06-10 11:58:56 -0400

    Add lazy loading form lua snippets

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -4,7 +4,7 @@ local lspkind = require 'lspkind'
 
 require('luasnip.loaders.from_snipmate').lazy_load()
 require('luasnip.loaders.from_vscode').lazy_load()
-require('luasnip.loaders.from_lua').load { paths = vim.fn.stdpath 'config' .. '/snippets' }
+require('luasnip.loaders.from_lua').lazy_load { paths = vim.fn.stdpath 'config' .. '/snippets' }
 
 luasnip.config.setup {}
 
