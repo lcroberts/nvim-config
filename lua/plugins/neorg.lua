5faff23fc2ed6a992b814a6058646583f617270b
Logan Roberts <logancroberts@outlook.com>
2024-07-07 17:35:10 -0400

    Fix luarock build

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -3,6 +3,11 @@ return {
     'vhyrro/luarocks.nvim',
     priority = 1000, -- We'd like this plugin to load first out of the rest
     config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+    opts = {
+      luarocks_build_args = {
+        '--with-lua-include=/usr/include',
+      },
+    },
   },
   {
     'nvim-neorg/neorg',
